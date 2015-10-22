97c5d8ed88ba64601c684ca2faec7b0e50cf525e
atanasg <atanas.gegov.oss@gmail.com>
2015-10-23 00:49:26 +0200

    Add class containing the names of the available commands

---
+++ FileProcessingApp/src/main/java/com/atanasg/fileprocessingapp/commands/CommandNames.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2015 Atanas Gegov
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.atanasg.fileprocessingapp.commands;
+
+/**
+ * The names of the supported commands in
+ * this application. These are the recognized
+ * strings when the user enters some command.
+ *
+ * @author Atanas Gegov
+ */
+public class CommandNames {
+
+	// swap commands
+	public static final String COMMAND_SWAPLINES = "swaplines";
+	public static final String COMMAND_SWAPNUMS = "swapnums";
+
+	// CRUD commands
+	public static final String COMMAND_INSERTNUM = "insertnum";
+	public static final String COMMAND_MODIFYNUM = "modifynum";
+	public static final String COMMAND_READNUM = "readnum";
+	public static final String COMMAND_REMOVENUM = "removenum";
+
+	// validate command
+	public static final String COMMAND_VALIDATE = "validate";
+
+	// save command
+	public static final String COMMAND_SAVE = "save";
+
+	// other commands
+	public static final String COMMAND_HELP = "help";
+	public static final String COMMAND_QUIT = "quit";
+
+}
